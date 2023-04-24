a826ce4bd3c14905a790957c0f492c1df35a33b5
Artūrs T <arturs@migbaltic.com>
2023-04-24 22:22:49 +0300

    Change queries route from get to post

--- src/AuditLog.php
+++ src/AuditLog.php
@@ -2,30 +2,54 @@
 
 namespace Atimrots\AuditLog;
 
+use GuzzleHttp\Exception\GuzzleException;
+use JsonException;
+
 class AuditLog {
 	private const POST = 'POST';
 	private const GET = 'GET';
-	private const POST_BASE_PATH = '/log/';
-	private const GET_BASE_PATH = '/get/';
+	private const STORE_BASE_PATH = '/log/';
+	private const SHOW_BASE_PATH = '/select/';
 
+	/**
+	 * @param string $collection
+	 * @param array $data
+	 *
+	 * @return void
+	 * @throws GuzzleException
+	 * @throws JsonException
+	 */
 	public static function post(string $collection, array $data): void {
 		$client = new Client();
 
 		// TODO: validate if collection exists from cached values
 
-		$response = $client->makeRequest(self::POST, self::POST_BASE_PATH.$collection, $data);
-
-		dd($response);
+		$client->makeRequest(self::POST, self::STORE_BASE_PATH.$collection, $data);
 	}
 
-	public static function get(string $collection, array $query = []): array {
+	/**
+	 * @param string $collection
+	 * @param array $query
+	 * @param array $body
+	 *
+	 * @return array
+	 * @throws GuzzleException
+	 * @throws JsonException
+	 */
+	public static function get(string $collection, array $query = [], array $body = []): array {
 		$client = new Client();
 
-		// TODO: validate if collection exists from cached values
+		// TODO: validate if collection exists using cached values
 
-		$response = $client->makeRequest(self::GET, self::GET_BASE_PATH.$collection, $query);
+		$path = self::SHOW_BASE_PATH.$collection;
 
-		dd(json_decode($response['response']['result'], true));
+		if ($query) {
+			$path .= '?'.http_build_query($query);
+		}
+
+		$response = $client->makeRequest(self::POST, $path, $body);
+
+		$response['response']['result'] = json_decode($response['response']['result'], true, 512, JSON_THROW_ON_ERROR);
 
 		return $response;
 	}
